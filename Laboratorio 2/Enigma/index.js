7765592a2a9fc79deeeb7b2ed58011354da211ce
Carlos Pizarro <122272599+IncorrigibleSpirit@users.noreply.github.com>
2023-04-20 16:45:07 +0900

    Updated index.js
    Included new arguments.
    Updated listeners (A and B)

--- Laboratorio 2/Enigma/index.js
+++ Laboratorio 2/Enigma/index.js
@@ -11,22 +11,20 @@ var searchIndex = (alphabet,target) =>{
         }
     }
 
-//5 Creacion de funcion transformadora (tocará crear la función "searchIndex")
-function transformLetter (letter,sourceString, destinyString) {  //letra, string desde el que deseo escribrir, string hacia donde codifico
-var letterIndex = searchIndex(sourceString,letter); //Llamamos funcion SearchIndex. Buscamos el indice de la letra en el alfabeto plano
+///5 Creacion de funcion transformadora (tocará crear la función "searchIndex")  
+function transformLetter (letter,sourceString, destinyString) { //letra, string desde el que deseo escribrir, string hacia donde codifico
+var letterIndex = searchIndex(sourceString,letter);//Llamamos funcion SearchIndex. Buscamos el indice de la letra en el alfabeto plano
 return destinyString[letterIndex];//5.2 Con el índice claro, ya podemos llamar la letra en el alfabeto encriptado. Y esta finalmente es la que se retorna.
-    
 }
 
-
 //1 Iterar el array para obtener el índice de cada letra (llega el mensaje)
-var iterateAndTransformText = text => {
-    var textLowerCase = text.toLowerCase(); //2 Pasamos el texto ingresado a minúscula
+var iterateAndTransformText = (text,originalAlphabet,destinyAlphabet)  => {
+    var textLowerCase = text.toLowerCase();//2 Pasamos el texto ingresado a minúscula
     var result = ""; //5 Aquí se almacenará resultado de función de transformadora 
     for (var letter of textLowerCase) { //3 Iteramos cada una de las letras de la cadena de texto
         //4 Creamos funcion transformadora. Le pasamos letra y devuelve la letra encriptada/transformada
         //6 El resultado de esta función se almacena en var = result. Inicia en 0. Después acumula
-        var LetsTransformLetter = transformLetter(letter,encryptedAlphabet,plainAlphabet);
+        var LetsTransformLetter = transformLetter(letter,originalAlphabet,destinyAlphabet);
         // El orden en que se envíen los argumentos influirá en el funcionamiento del código 
         // No funciona a la vez encriptación y desencriptación.
         result = result + LetsTransformLetter;
@@ -39,15 +37,20 @@ iterateAndTransformText(userTextBoxB());
 
 //LISTENERS
 
+//Clave del ejercicio:Al cierre del ejercicio se presentaban problemas con el funcionamiento.
+//Desde el listener se suministraorn a la funcion principal, no solo el texto por el usuario.
+//También se pasaron el alfabeto original y alfabeto de destino.El orden cambia para cada boton.
+//Desde aquí SEÑALAMAOS QUE LE PASAREMOS A LA FUNCION PRINCIPAL, y en qué orden será recibido.
+
 var botonEncriptar = document.getElementById("encriptar");
 botonEncriptar.addEventListener("click",(event) => {
 event.preventDefault();
-document.getElementById("boxB").innerText = iterateAndTransformText(userTexBoxA())
+document.getElementById("boxB").innerText = iterateAndTransformText(userTexBoxA(),plainAlphabet,encryptedAlphabet)
 })
 
 
 var botonDesencriptar = document.getElementById("desencriptar");
 botonDesencriptar.addEventListener("click",(event) => {
 event.preventDefault();
-document.getElementById("boxA").innerText = iterateAndTransformText(userTextBoxB())
+document.getElementById("boxA").innerText = iterateAndTransformText(userTextBoxB(),encryptedAlphabet,plainAlphabet)
 })
