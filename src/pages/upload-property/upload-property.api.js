8d739398433543577796c95dcae69f6e6252db1c
IncorrigibleSpirit <contacto.carlospizarro@gmail.com>
2023-11-14 23:54:41 +0900

    Deleted version 1 -  Unidad 13 - portal inmobiliario

--- src/pages/upload-property/upload-property.api.js
+++
@@ -1,9 +0,0 @@
-import Axios from 'axios';
-
-const saleTypeListUrl = `${process.env.BASE_API_URL}/saleTypes`;
-export const getSaleTypeList = () =>
-Axios.get(saleTypeListUrl).then(({ data }) => data);
-
-const provinceListUrl = `${process.env.BASE_API_URL}/provinces`;
-export const getProvinceList = () =>
-Axios.get(provinceListUrl).then(({ data }) => data);
