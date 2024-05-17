a6c0ddd864b9d4c9c4ec2380be6c12db3ea5390d
Carlos Pizarro <122272599+IncorrigibleSpirit@users.noreply.github.com>
2024-05-17 13:51:34 +0900

    Update Laboratorio intermedio - nomina-deducciones.js

--- Laboratorio intermedio - nomina-deducciones.js
+++ Laboratorio intermedio - nomina-deducciones.js
@@ -1,9 +1,11 @@
 const empleado = {
-      bruto:14.500,
-      hijos:2,
-      pagas:14
-    }
-    
+  bruto: 14.5,
+  hijos: 2,
+  pagas: 14,
+};
+
+/* VERSION ORIGINAL (LARGA)
+
     if (empleado.bruto <= 12.000){
 
         let retencion = 0
@@ -82,3 +84,30 @@ const empleado = {
         let totalNetoMensual = totalNetoAnual / empleado.pagas;
         console.log(totalNetoMensual *1000);
     }
+
+*/
+
+//VERSION SIMPLIFICADA
+
+let retencion;
+
+if (empleado.bruto <= 12.0) {
+  retencion = 0;
+} else if (empleado.bruto <= 24.0) {
+  retencion = empleado.hijos > 0 ? 0.06 : 0.08;
+} else if (empleado.bruto <= 34.0) {
+  retencion = empleado.hijos > 0 ? 0.14 : 0.16;
+} else {
+  retencion = empleado.hijos > 0 ? 0.28 : 0.3;
+}
+
+console.log(retencion);
+let retencionAplicada = empleado.bruto * retencion;
+console.log(retencionAplicada);
+
+let totalNetoMensual = empleado.bruto - retencionAplicada;
+console.log(totalNetoMensual.toFixed(3));
+
+let totalNetoAnual = totalNetoMensual * empleado.pagas;
+console.log(totalNetoAnual.toFixed(3));
+
