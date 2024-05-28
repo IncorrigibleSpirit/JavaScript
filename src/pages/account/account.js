dab66c6ed336149287c679f757f0c2d091a311db
Carlos Pizarro <122272599+IncorrigibleSpirit@users.noreply.github.com>
2024-05-28 20:35:47 +0900

    Update account.js

--- src/pages/account/account.js
+++ src/pages/account/account.js
@@ -10,20 +10,23 @@ import { history } from '../../core/router';
 import { insertAccount, getAccount, updateAccount } from './account.api';
 import { mapAccountVmToApi, mapAccountApiToVm } from './account.mappers';
 
-//1 CAPTURAMOS EL ID DE LA URL, SI ES TRUE, OBTENEMOS CUENTA, CONVERTIMOS
-// A VM, Y FINALMENTE CONECTAMOS AL HTML (PARA QUE SE VEA EN PANTALLA)
+/*COMENTARIO - ACLARACION
+QUE SON LOS "PARAMS" (query parameters) o que puntualmente? los "parámetros de la URL" se refiere a las partes de la URL que vienen después del signo de interrogación (?)
+ y que generalmente se usan para pasar información adicional. Estos parámetros están formateados como pares clave-valor y se conocen comúnmente como "query parameters" 
+o "parámetros de consulta. Ejemplo: https://example.com/page?name=JohnDoe&id=123.  Los parámetros de la URL son name=JohnDoe y id=123. La funcion getParams() 
+toma estos parámetros y los convierte en un objeto de JavaScript  {name: 'JohnDoe', id: '123'}*/
 
-//Modelo de vista - en caso de crear cuenta
+//0 Modelo de vista - en caso de crear cuenta
 let account = {
   id: '',
   type: '',
   alias: '',
 };
 
-/* QUE SON LOS PARAMS, O QUE PUNTUALMENTE TOMA? los "parámetros de la URL" se refiere a las partes de la URL que vienen después del signo de interrogación (?)
- y que generalmente se usan para pasar información adicional a la página web. Estos parámetros están formateados como pares clave-valor y se conocen comúnmente 
-como "query parameters" o "parámetros de consulta. Ejemplo: https://example.com/page?name=JohnDoe&id=123.  Los parámetros de la URL son name=JohnDoe y id=123. 
-La funcion getParams() toma estos parámetros y los convierte en un objeto de JavaScript  {name: 'JohnDoe', id: '123'}*/
+/*4 El siguiente procedimiento permite la captura del parametro de consulta (query parameter), si es "True", procedemos a tomar LA INFO DE LA CUENTA haciendo uso del metodo
+ de la API "getAccount" (GET). De esta extraccion tendremos dos campos de datos (tipo de cuenta y alias), los cuales seran ajustados por el mapper. 
+Es de señalar, que esta informacion se muestra en pantalla tan pronto el usuario hace clic en el numero de una de las cuentas listadas (ES91 2100 0418 4502 0005 1332) 
+en la pagina account-list. Finalmente conectamos la informacion al HTML usando "onSetValues".*/
 
 const params = history.getParams();
 const isEditMode = Boolean(params.id);
@@ -35,7 +38,7 @@ if (isEditMode) {
   });
 }
 
-// CAPTURA Y ACTUALIZACIÓN DE CAMPO 1
+// 1 Captura y validacion de campo type
 onUpdateField('type', (event) => {
   const value = event.target.value;
   account = { ...account, type: value };
@@ -45,7 +48,7 @@ onUpdateField('type', (event) => {
   });
 });
 
-// CAPTURA Y ACTUALIZACIÓN DE CAMPO 2
+// 2 Captura y validacion de campo alias
 onUpdateField('alias', (event) => {
   const value = event.target.value;
   account = { ...account, alias: value };
@@ -55,21 +58,23 @@ onUpdateField('alias', (event) => {
   });
 });
 
-/* FUNCION CLAVE: ONSAVE
-CONVERTIMOS DATOS VM A API ("LOS DEVOLVEMOS"). SI
-TIENE ID, ACTUALIZA LA INFO, DE LO CONTARIO, CREA UNA NUEVA CUENTA.*/
 
+/* 5 Funcion Clave (onsave): Nos disponemos a guardar informacion que se ha diligenciado en el formulario ( Ya sea que se haya ingresado por el numero de IBAN, 
+o se haya dado click en el boton "crear nueva cuenta"). Inicialmente la informacion se transformará con el mapper para que se ajuste a la data del servidor, 
+luego se preguntará si es editmode. Si tiene parametro de consulta ID, se actualiza info. De lo contrario, se crea nueva cuenta.*/
 const onSave = () => {
   const apiAccount = mapAccountVmToApi(account);
   return isEditMode ? updateAccount(apiAccount) : insertAccount(apiAccount);
 };
 
-// PROCESAMIENTO DE BOTON
+
+// 3 captura de boton y validacion de formulario
 onSubmitForm('save-button', () => {
   //console.log({ account });
   formValidation.validateForm(account).then((result) => {
     onSetFormErrors(result);
 
+// 6 Si es existoso todo, procedemos a salvar y volver a la pagina account-list
     if (result.succeeded) {
       onSave().then(() => {
         history.back();
@@ -77,4 +82,3 @@ onSubmitForm('save-button', () => {
     }
   });
 });
-
